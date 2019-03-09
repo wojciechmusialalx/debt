8b08cad438d639ba132c5d265387fa7108b51b25
Wojciech Musiał <wojciech_musial@poczta.fm>
2019-03-09 11:22:36 +0100

    initial commit

---
+++ src/main/java/pl/alx/debt/DebtApplication.java
@@ -0,0 +1,13 @@
+package pl.alx.debt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/alx/debt/DebtApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.alx.debt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DebtApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
